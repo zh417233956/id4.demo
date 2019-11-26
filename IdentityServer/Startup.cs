f8e5d186f1064f505a5fcde24493cde74d1f276e
钟海 <417233956@qq.com>
2019-11-26 11:40:25 +0800

    创建一个MVC客户端

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -26,7 +26,8 @@ namespace IdentityServer
             var builder = services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.Ids)
                 .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients);
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
