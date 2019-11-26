9f192c1164a6953b23473738d0e6840bfceac6f7
钟海 <417233956@qq.com>
2019-11-26 11:23:06 +0800

    添加测试用户

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -12,7 +13,9 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
             {
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                //添加姓名等资源授权
+                new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiResource> Apis =>
@@ -39,6 +42,28 @@ namespace IdentityServer
 
                 // scopes that client has access to
                 AllowedScopes = { "api1" }
+            },
+             // interactive ASP.NET Core MVC client
+            new Client
+            {
+                ClientId = "mvc",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                RequireConsent = true,//是否弹出授权
+                RequirePkce = true,
+
+                // where to redirect to after login
+                RedirectUris = { "http://localhost:5001/signin-oidc","http://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc","http://localhost:5002/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
             }
         };
 
