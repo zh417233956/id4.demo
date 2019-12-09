349d3c733d1937be1e8dd2b113ddefb8d459c675
钟海 <417233956@qq.com>
2019-12-09 18:31:49 +0800

    集成实现keycloak的oidc鉴权

---
+++ WebAPI/Auths/CustomsAuthorizationHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Auths
+{
+    public class CustomsAuthorizationHandler : AuthorizationHandler<CustomsAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomsAuthorizationRequirement requirement)
+        {
+            if (!context.User.Identity.IsAuthenticated)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var resource_access = context.User.Claims.FirstOrDefault(m => m.Type == "resource_access")?.Value;
+            JObject resource_access_json = JObject.Parse(resource_access);
+
+            var roles_json = resource_access_json.First.FirstOrDefault()?.FirstOrDefault()?.FirstOrDefault();
+            List<string> roles = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(roles_json.ToString());
+
+            if (roles.Contains("admin"))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+            context.Fail();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebAPI.Auths;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = Configuration["Id4:Authority"];
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = Configuration["Id4:Audience"];
+
+                   
+                    //基于claim的鉴权
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        RoleClaimType = "role",
+                        NameClaimType= "preferred_username",                        
+                    };
+                });
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            //services.AddAuthorization(options => { options.AddPolicy("Users", policy => policy.RequireRole("Users")); });
+
+            services.AddAuthorization(option => {
+                option.AddPolicy("customs", builder =>
+                {
+                    //自定义Policy
+                    builder.AddRequirements(new CustomsAuthorizationRequirement());
+                });
+            });
+            //注入自定义Policy
+            services.AddSingleton<IAuthorizationHandler, CustomsAuthorizationHandler>();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //UseAuthentication
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
